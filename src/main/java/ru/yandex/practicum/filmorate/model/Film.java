14dec0db96ad39c5708aecbe69e59e2595657619
Daggett <adsbvb@yandex.ru>
2025-05-20 01:18:54 +0500

    Добавить ограничительные аннотации в поля класса

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,16 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.validation.ReleaseDateConstraint;
 
 import java.time.LocalDate;
 
 @Data
 public class Film {
     private Long id;
+    @NotBlank(message = "Название не может быть пустым")
     private String name;
+    @Size(min = 1, max = 200, message = "Максимальная длина описания — 200 символов")
     private String description;
+    @NotNull(message = "Дата релиза должна быть указана")
+    @ReleaseDateConstraint(message = "Дата релиза — не раньше 28 декабря 1895 года")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
+    @Min(value = 1, message = "Продолжительность фильма должна быть положительным числом")
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,16 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.validation.UserBirthdayDateConstraint;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
     private Long id;
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Некорректный адрес электронной почты")
     private String email;
+    @NotBlank(message = "Логин не может быть пустым")
     private String login;
     private String name;
+    @UserBirthdayDateConstraint(message = "Дата рождения не может быть в будущем")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }
