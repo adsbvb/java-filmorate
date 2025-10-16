7900135c60432968ef2cf322ee689546730cd4b6
Danil Shirykalov <shirds@yandex.ru>
2025-10-16 21:26:38 +0500

    Удалены неиспользуемые импорты.

--- src/main/java/ru/yandex/practicum/filmorate/dal/GenreJdbcStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/GenreJdbcStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.dal;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.dal.mappers.GenreRowMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 
