7b448cb3ed7c8b03c2db8c0e431e51db95e49e02
Gospod9boga <didovecv@mail.ru>
2025-10-19 18:47:33 +0300

    fix: checkstyle whitespace around {

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -79,8 +79,8 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long id){
-        log.info("Получен запрос на удаления пользователя {}",id);
+    public void deleteUser(@PathVariable Long id) {
+        log.info("Получен запрос на удаления пользователя {}", id);
         userService.deleteUser(id);
     }
 
