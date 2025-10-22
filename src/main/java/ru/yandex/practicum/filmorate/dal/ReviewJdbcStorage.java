c4eba56320447869e42cd8c9e0a9cf47d7a5f21f
adsbvb <adsbvb@yandex.ru>
2025-10-22 23:50:02 +0500

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/ReviewJdbcStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/ReviewJdbcStorage.java
@@ -5,7 +5,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exception.InternalServerException;
 import ru.yandex.practicum.filmorate.model.Review;
 
 import java.util.List;
